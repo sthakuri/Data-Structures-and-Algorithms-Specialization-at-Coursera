0c320a24fce9a69b401a70de166b1650858c534a
Suvash Shah Thakuri <suvashshah.thakuri@gmail.com>
2017-02-15 22:53:11 -0800

    Product Fast Algorith applied

--- Week1/Max Pairwise Product/MaxPairwiseProduct.cs
+++ Week1/Max Pairwise Product/MaxPairwiseProduct.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
-public class MaxPairwiseProduct {
-    static int getMaxPairwiseProduct(int[] numbers) {
-        int result = 0;
+public class MaxPairwiseProduct
+{
+    static Int64 getMaxPairwiseProduct(int[] numbers)
+    {
+        Int64 result = 0;
         int n = numbers.Length;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (numbers[i] * numbers[j] > result) {
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                if (numbers[i] * numbers[j] > result)
+                {
                     result = numbers[i] * numbers[j];
                 }
             }
@@ -14,13 +19,33 @@ public class MaxPairwiseProduct {
         return result;
     }
 
-    public static void Main(String[] args) {        
+    static Int64 getMaxPairwiseProductFast(int[] numbers)
+    {        
+        int n = numbers.Length;
+        int max_index1 = -1;
+        for (int i = 0; i < n; ++i)
+            if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+                max_index1 = i;
+
+        int max_index2 = -1;
+        for (int j = 0; j < n; ++j)
+            if ((numbers[j] != numbers[max_index1]) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+                max_index2 = j;
+
+        return ((Int64)(numbers[max_index1])) *numbers[max_index2];        
+    }
+
+
+    public static void Main(String[] args)
+    {
         int n = Convert.ToInt32(Console.ReadLine());
-        string[] strNums= Console.ReadLine().Split(' ');        
+        string[] strNums = Console.ReadLine().Split(' ');
         int[] numbers = new int[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] =Int32.Parse(strNums[i]);
+        for (int i = 0; i < n; i++)
+        {
+            numbers[i] = Int32.Parse(strNums[i]);
         }
         Console.WriteLine(getMaxPairwiseProduct(numbers));
+        Console.WriteLine(getMaxPairwiseProductFast(numbers));
     }
 }
