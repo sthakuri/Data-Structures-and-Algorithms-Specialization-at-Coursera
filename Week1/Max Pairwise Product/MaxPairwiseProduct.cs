f9a1a76ad4c9e52fc8ca428ed7196f46ae9a8973
Suvash Shah Thakuri <suvashshah.thakuri@gmail.com>
2017-02-15 22:43:48 -0800

    initial

---
+++ Week1/Max Pairwise Product/MaxPairwiseProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class MaxPairwiseProduct {
+    static int getMaxPairwiseProduct(int[] numbers) {
+        int result = 0;
+        int n = numbers.Length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (numbers[i] * numbers[j] > result) {
+                    result = numbers[i] * numbers[j];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void Main(String[] args) {        
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] strNums= Console.ReadLine().Split(' ');        
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] =Int32.Parse(strNums[i]);
+        }
+        Console.WriteLine(getMaxPairwiseProduct(numbers));
+    }
+}
