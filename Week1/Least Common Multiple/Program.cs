5b7731350d90d420a3a476ac8bdf254db0c25409
Suvash Shah Thakuri <suvashshah.thakuri@gmail.com>
2017-02-16 22:51:12 -0800

    initial commit

---
+++ Week1/Least Common Multiple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(LCM(12,80));
+            Console.WriteLine(LCM(12,8));
+        }
+
+        static long LCM(int a, int b)
+        {
+            return (long)a*b/GCD(a,b);
+        }
+        static int GCD(int a, int b)
+        {
+            int mod = int.MaxValue;
+            while (mod > 0)
+            {
+                if (a > b)
+                {
+                    mod = a % b;
+                    a = mod;
+                }
+                else if (a < b)
+                {
+                    mod = b % a;
+                    b = mod;
+                }
+                else
+                    mod = 0;
+            }
+            return a == 0 ? b : a;
+        }
+    }
+    
+}
